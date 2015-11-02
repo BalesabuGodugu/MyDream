a3a437a4ad08716fbfafdae38c8173ba7db16a29
Balesabu <godugubalesabu@gmail.com>
2015-11-03 00:56:55 +0530

    added script files and css files

---
+++ js/scripts.js
@@ -0,0 +1,34 @@
+
+$(document).ready(function(){/* activate scrollspy menu */
+$('body').scrollspy({
+  target: '#navbar-collapsible',
+  offset: 50
+});
+
+/* smooth scrolling sections */
+$('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top - 50
+        }, 1000);
+        return false;
+      }
+    }
+});
+
+  $(function(){
+      $(".element").typed({
+        strings: ["Make your Journey Success", "With JS"],
+        typeSpeed: 0
+      });
+  });
+
+
+
+
+
+
+});
